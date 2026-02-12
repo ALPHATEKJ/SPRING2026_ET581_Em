f1743ee18e27326289932d5ec66dde8611592650
ALPHATEKJ <ellianmoreira123@gmail.com>
2026-02-12 01:25:26 +0000

    update

---
+++ lab1/ex3.java
@@ -0,0 +1,31 @@
+public class ex3 {
+    public static void main(String[] args) {
+        
+                                                    // so we begin with (i = 10) so boom point blank 10. 
+                                                    //then we end it with ";" and then delcare after that if 10 is greater than or equal to 1; we subtract 1 from 10 hence i-- then we close the statement with the close ")"
+         //(int i = 10; i >= 1; i--)  { 
+           // System.out.println(i); 
+       // }
+        ex3();
+    }
+
+
+
+    public static void ex3()  { 
+            int i = 10 ; 
+            while (i >= 1){
+                System.out.println("test: " + i );
+                i--;
+            }
+
+
+
+
+
+    }
+}
+
+    
+
+
+
